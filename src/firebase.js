5ddd2b3077c6c4ef26ae2015f978dc2104dfc093
ashhar55 <134512836+ashhar55@users.noreply.github.com>
2023-10-10 12:53:47 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAaU6CHE7jTQmhIfXQcExllOyefC_uVERQ",
-  authDomain: "otp-verification-53d87.firebaseapp.com",
-  projectId: "otp-verification-53d87",
-  storageBucket: "otp-verification-53d87.appspot.com",
-  messagingSenderId: "669099969783",
-  appId: "1:669099969783:web:b6ca28819556538d3197f2"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
